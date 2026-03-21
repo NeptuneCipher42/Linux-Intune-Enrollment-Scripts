b66c8d1383f2b1f391c905c274090cfb215bbc6c
NeptuneCipher42 <143345524+NeptuneCipher42@users.noreply.github.com>
2026-03-21 19:44:29 -0400

    Update Linux-Enrollment-Azure-VM.sh

--- Linux-Enrollment-Azure-VM.sh
+++ Linux-Enrollment-Azure-VM.sh
@@ -6,7 +6,7 @@ sudo apt-get update && apt-get full-upgrade -y
 
 #Install and enable GNOME
 # Install GNOME + Flashback session
-sudo apt install -y ubuntu-desktop-minimal gnome-session-flashback metacity xrdp xorgxrdp dbus-x11
+sudo apt-get install -y ubuntu-desktop-minimal gnome-session-flashback metacity xrdp xorgxrdp dbus-x11
 
 # Enable XRDP
 sudo systemctl enable --now xrdp
@@ -38,7 +38,7 @@ sudo curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > mi
 sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/
 rm microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/ubuntu/22.04/prod jammy main" > /etc/apt/sources.list.d/microsoft-ubuntu-jammy-prod.list'
-sudo apt update
+sudo apt-get update
 sudo apt install -y intune-portal
 
 sudo reboot
